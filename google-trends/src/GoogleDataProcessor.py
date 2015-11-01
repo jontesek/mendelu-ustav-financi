394d6d5b8f8290a8c38536d281608a9239a6382f
Jonas Petrovsky <jond@post.cz>
2015-11-01 09:54:27 +0100

    monthly count output file working

--- google-trends/src/GoogleDataProcessor.py
+++ google-trends/src/GoogleDataProcessor.py
@@ -24,17 +24,36 @@ class GoogleDataProcessor(object):
         # Read the country sheet
         in_sheet = self.in_workbook[country_code]
         # Create a new empty sheet
-        self.out_workbook.create_sheet(country_code)
+        country_sheet = self.out_workbook.create_sheet(country_code)
         # Browse all columns (search terms)
+        country_data = []
         for col in in_sheet.columns:
             # Check if an empty column was reached.
             if not col[0].value:
                 break    # stop the loop
-            self._process_search_word(col)
+            country_data.append(self._process_search_word(col))
             if col[0].value > 1:
                 break
+        # Write data to the sheet
+        for col_idx, (word_id, word_string, word_data) in enumerate(country_data):
+            col_num = col_idx + 2
+            # Write word id and word string
+            country_sheet.cell(column=col_num,row=1).value = word_id
+            country_sheet.cell(column=col_num,row=2).value = word_string
+            # Check if there are any data
+            if not word_data:
+                continue
+            # If yes, write periods data.
+            for row_idx, (period_name, avg_num) in enumerate(word_data):
+                country_sheet.cell(column=col_num,row=row_idx+3).value = avg_num
+
+        # Save file
+        out_filename = path.abspath(self.file_paths['output_dir']+'/period_data.xlsx')
+        self.out_workbook.save(out_filename)
+
 
     def _process_search_word(self, column):
+        # Get word info
         word_id = column[0].value
         word_string = column[1].value
         # Check if the values have monthly or weekly frequency.
@@ -44,15 +63,18 @@ class GoogleDataProcessor(object):
         else:
             print('month')
             word_data = self._process_monthly_values(3, column[2:])
-            return 0
-        # Write the counted data to the output workbook.
-
-
+        # result
+        #print word_data
+        return [word_id, word_string, word_data]
 
     def _process_weekly_values(self, items):
-        pass
+        return False
 
     def _process_monthly_values(self, months_period, items):
+        # Check if the first item is empty - data not present.
+        if not items[0].value:
+            return False
+        # Otherwise, process all data.
         word_data = []
         period = {'year': None, 'sum': 0, 'number': 1}
         for index, item in enumerate(items):
@@ -68,10 +90,10 @@ class GoogleDataProcessor(object):
             period['year'] = result_year
             # Check if the current period should end (there are 3 months in a quarter of a year).
             if long(index+1) % months_period == 0:
-                print period
+                #print period
                 # If yes, count the average score of the word and save it with the period ID.
                 period_avg = period['sum'] / months_period
-                period_name = '%s-%s' % (period['year'], period['number'])
+                period_name = '%sq%s' % (period['year'], period['number'])
                 word_data.append((period_name, period_avg))
                 # Reset the period
                 period = {'year': None, 'sum': 0, 'number': period['number']+1}
@@ -79,5 +101,4 @@ class GoogleDataProcessor(object):
                 if period['number'] == 5:
                     period['number'] = 1
         # return data
-        print word_data[0]
         return word_data
