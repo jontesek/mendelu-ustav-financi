6243baecc8f028a44a4734edef461696ba4de0a3
Jonas Petrovsky <jond@post.cz>
2016-07-02 11:32:48 +0200

    init commit for data getter

---
+++ google-trends/src/GoogleDataGetter.py
@@ -0,0 +1,21 @@
+import pytrends
+
+class GoogleDataGetter(object):
+
+    def __init__(self, credintals):
+        pass
+
+    def get_data_for_country(self, words_list):
+        """
+        Args:
+            words_list (list): simple list of words to search.
+
+        Returns:
+            data_list (list): list of {'term', 'values'} dicts.
+        """
+
+        
+
+
+
+
